7d9053bb068e00d42c71a6a4211297e7fd458da1
Jesús Espino <jespinog@gmail.com>
2015-03-11 18:11:08 +0100

    Less dependency on werkzeug code

--- anillo/middlewares/cookies.py
+++ anillo/middlewares/cookies.py
@@ -1,5 +1,5 @@
 from werkzeug.http import dump_cookie
-from werkzeug._compat import to_unicode, text_type, try_coerce_native
+from werkzeug._compat import to_unicode
 from werkzeug._internal import _cookie_parse_impl
 
 
@@ -8,14 +8,14 @@ def _parse_cookie(header, charset='utf-8', errors='replace'):
     # is done because on PEP 3333 on Python 3 all headers are assumed latin1
     # which however is incorrect for cookies, which are sent in page encoding.
     # As a result we
-    if isinstance(header, text_type):
+    if isinstance(header, str):
         header = header.encode('latin1', 'replace')
 
     def _parse_pairs():
         for key, val in _cookie_parse_impl(header):
             key = to_unicode(key, charset, errors, allow_none_charset=True)
             val = to_unicode(val, charset, errors, allow_none_charset=True)
-            yield try_coerce_native(key), {"value": val}
+            yield key, {"value": val}
 
     return dict(_parse_pairs())
 
