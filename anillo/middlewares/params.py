a60c93a66af6b029f69214eb6a2b52200889c776
Jesús Espino <jespinog@gmail.com>
2015-03-12 22:33:02 +0100

    Improved the form_params middleware

--- anillo/middlewares/params.py
+++ anillo/middlewares/params.py
@@ -5,6 +5,13 @@ from urllib.parse import parse_qs
 def form_params_middleware(func):
     def wrapper(request):
         if request.headers.get("Content-Type", '') == "application/x-www-form-urlencoded":
-            request.form_params = parse_qs(request.body.decode("utf-8"))
+            request.form_params = {}
+            for key, value in parse_qs(request.body.decode("utf-8")).items():
+                if len(value) == 0:
+                    request.form_params[key] = None
+                elif len(value) == 1:
+                    request.form_params[key] = value[0]
+                else:
+                    request.form_params[key] = value
         return func(request)
     return wrapper
