8fbd017ea55fb975dd5424a96c859aaad87cb585
Jesús Espino <jespinog@gmail.com>
2015-04-21 16:12:09 +0200

    Initial multipart support

---
+++ anillo/middlewares/multipart_params.py
@@ -0,0 +1,22 @@
+import json
+from urllib.parse import parse_qs
+from cgi import parse_multipart, parse_header
+from io import BytesIO
+
+
+def multipart_params_middleware(func):
+    def wrapper(request):
+        ctype, pdict = parse_header(request.headers.get('Content-Type', ''))
+        if ctype == "multipart/form-data":
+            if isinstance(pdict['boundary'], str):
+                pdict['boundary'] = pdict['boundary'].encode()
+            post_data = parse_multipart(BytesIO(request.body), pdict)
+            print(post_data)
+            request.multipart_params = {}
+            for key, value in post_data.items():
+                if len(value) == 1:
+                    request.multipart_params[key] = value[0]
+                else:
+                    request.multipart_params[key] = value
+        return func(request)
+    return wrapper
---
+++ tests/middlewares/test_multipart_params.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+    tests.middlwares.test_params
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Tests params middlewares
+
+    :copyright: (c) 2015 by Jesús Espino.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from anillo.middlewares.multipart_params import multipart_params_middleware
+from anillo.http.request import Request
+from anillo.http.responses import Response
+
+
+@multipart_params_middleware
+def multipart_app(request):
+    return Response()
+
+
+def test_parse_multipart_params():
+    file_test_1 = ""
+    file_test_2 = ""
+    request = Request(
+        body=b"""
+----test
+Content-Disposition: form-data; name=test1
+
+test-value1
+----test
+Content-Disposition: form-data; name=test2
+
+test-value2
+----test--""",
+        headers={"Content-Type": "multipart/form-data; boundary=--test"}
+    )
+    multipart_app(request)
+    assert "test1" in request.multipart_params
+    assert request.multipart_params["test1"] == b"test-value1"
+    assert "test2" in request.multipart_params
+    assert request.multipart_params["test2"] == b"test-value2"
+    assert "test3" not in request.multipart_params
