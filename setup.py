8facddbfd5b990479c2c89fceb6b7866fbcf2dcb
Andrey Antukh <niwi@niwi.nz>
2015-08-15 17:04:55 +0300

    Improve setup.py style and use proper version numbering.
    The 0.0.1 is a first patch version of not existent 0.0.0 version.
    In this commit, we start using semantic versioning with first
    version to 0.1.0.

--- setup.py
+++ setup.py
@@ -4,22 +4,19 @@
 from setuptools import setup
 
 setup(
-    name='anillo',
-    version=":versiontools:anillo:",
-    description="Ring/compojure like nanoframework build on top of werkzoug",
-    long_description="",
-    keywords='framework, web, ring, compojure',
-    author='Jesús Espino García',
-    author_email='jespinog@gmail.com',
-    url='https://github.com/jespino/anillo',
-    license='BSD',
-    packages=['anillo'],
+    name = "anillo",
+    version = "0.1.0",
+    description = "Ring/compojure like nanoframework build on top of werkzoug",
+    long_description = "",
+    keywords = "framework, web, ring, compojure",
+    author = "Jesús Espino García",
+    author_email = "jespinog@gmail.com",
+    url = "https://github.com/jespino/anillo",
+    license = "BSD",
+    packages = ["anillo"],
     install_requires=[
-        'werkzeug',
         'multipart',
-    ],
-    setup_requires=[
-        'versiontools >= 1.9.1',
+        "werkzeug",
     ],
     classifiers=[
         "Programming Language :: Python",
