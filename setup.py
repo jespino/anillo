6540ec898c2dd90fb19709dd04169cbb3eced571
Jesús Espino <jespinog@gmail.com>
2016-01-15 10:21:10 +0100

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "anillo",
-    version = "0.1.0",
+    version = "0.1.1",
     description = "Ring/compojure like nanoframework build on top of werkzeug",
     long_description = "",
     keywords = "framework, web, ring, compojure",
